b7509ad1c95252b79f61dbf164c116c76e0228da
Shaad0911 <shadabkhanbpl939@gmail.com>
2025-04-11 02:40:45 +0530

    Create simple_chatbot_with_groq_langchain.py

---
+++ simple_chatbot_with_groq_langchain.py
@@ -0,0 +1,23 @@
+import getpass
+import os
+from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+from langchain_core.messages import HumanMessage, SystemMessage
+
+load_dotenv()
+
+
+if __name__ == '__main__':
+    if not os.environ.get("GROQ_API_KEY"):
+     os.environ["GROQ_API_KEY"] = getpass.getpass("Enter API key for GROQ_API_KEY: ")
+
+    model = init_chat_model("llama3-8b-8192", model_provider="groq")
+
+
+    messages = [
+        SystemMessage("Translate the following text from English to Hindi:"),
+        HumanMessage("hi!"),
+    ]
+
+    response = model.invoke(messages)
+    print(response.content)
